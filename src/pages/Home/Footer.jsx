fd610a832305c446af672973deb863b8e82e087a
josef.karakoca <josef.karakoca@valtech.com>
2019-11-09 22:18:50 +0100

    changed logo color and added a logo to footer

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Row, Col } from 'antd';
 
+const logo = require('./../../common/assets/poddhype-logo-white-small.png');
+
 function Footer() {
   return (
     <footer id="footer" className="dark">
@@ -30,7 +32,11 @@ function Footer() {
         </Row>
       </div>
       <Row className="bottom-bar">
-        <Col md={4} sm={24} />
+        <Col md={4} sm={24} >
+          <span className="foter-logo">
+            <img style={{ height: '24px', marginLeft: '90px' }} alt="logo" src={logo} />
+          </span>
+        </Col>
         <Col md={20} sm={24}>
           <span style={{ marginRight: 12 }}>Copyright © <FormattedMessage id="app.footer.company" /></span>
         </Col>

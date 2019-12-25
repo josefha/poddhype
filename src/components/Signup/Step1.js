563acabb6b19044761ed4bcb10d01a85ae4d5b54
josefha <josef.karakoca@valtech.com>
2019-12-25 14:23:09 +0100

    moved terms of use to modal to component

--- src/components/Signup/Step1.js
+++ src/components/Signup/Step1.js
@@ -7,9 +7,9 @@ import "firebase/auth";
 import { addPodcastProfileInfo } from "../../common/api/db/podcastProfile"
 
 import { Spin, Typography, Divider, Input, Checkbox, Modal } from 'antd'
-import { Link } from "@reach/router"
 import { DefaultButton } from '../../common/components/Buttons'
 import { getFirebase } from "../../common/api/firebase"
+import TermsOfUse from '../TermsOfUse';
 
 
 
@@ -134,40 +134,13 @@ export default class Step1 extends React.Component {
     render() {
         return (
             <div className='form-content' >
-                <Modal
-                    bodyStyle={{ fontSize: '10px' }}
-                    title="Terms and Conditions"
-                    visible={this.state.showTerms}
-                    onOk={this.handleOk}
-                    onCancel={this.hideTerms}
-                >
-                    <h2><strong>Welcome to poddhype.com</strong></h2>
-
-                    <h2><strong>iFrames</strong></h2>
-
-                    <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
-
-                    <h2><strong>Cookies</strong></h2>
-
-                    <p>We employ the use of cookies. By accessing poddhype.com, you agreed to use cookies in agreement with the Poddhype's Privacy Policy.</p>
-
-                    <h2><strong>Privacy Policy</strong></h2>
-                    <h2> What information do we collect?</h2>
-                    <p>In Short: We collect personal information that you provide to us such as name, address, contact information, passwords and security data, and information about your podcast that you enter in the signup.
-                    We collect personal information that you voluntarily provide to us when registering at the Services expressing an interest in obtaining information about us or our products and services, when participating in activities on the Services (such as posting messages in our online forums or entering competitions, contests or giveaways) or otherwise contacting us.
-                    The personal information that we collect depends on the context of your interactions with us and the Services, the choices you make and the products and features you use. The personal information we collect can include the following:
-                    Personal Information Provided by You. We collect app usage, and other similar data.
-                    Credentials. We collect passwords, password hints, and similar security information used for authentication and account access.
-                    All personal information that you provide to us must be true, complete and accurate, and you must notify us of any changes to such personal information.
-                    We store cookies for analytics on how our users use our site and to keep track if you are a returning visitor.
-                    </p>
-                    <h2> Why do we store this information? </h2>
-                    <p> Your information is used on our platform to show display your profile. Your email will not be shared or displayed on the website.
-                        We will not sell your data to any third party. We have the right to use your podcast name and podcast image in marketing for poddhype.com unless you have told us otherwise.
-                        You can at any time tell us you don't want us to store your information, you can do so by sening an email to info@podhype.com.
-                    </p>
-                </Modal>
-                <Spin spinning={this.state.isLoading} tip="Skapar konto...">
+                <Spin spinning={this.state.isLoading}
+                    tip="Skapar konto...">
+                    <TermsOfUse
+                        showTerms={this.state.showTerms}
+                        handleOk={this.handleOk}
+                        hideTerms={this.hideTerms}
+                    />
                     <Title style={{ textAlign: 'center' }} level={2}>Bli en partnerpodd</Title>
                     <p>Beskriv din podd och dina mål så sköter vi resten.</p>
                     <Divider />
--- src/components/TermsOfUse/index.js
+++ src/components/TermsOfUse/index.js
@@ -1,61 +1,44 @@
 
 import React from 'react'
 import "../Signup/style.less"
+import { Modal } from 'antd'
 
-import { Layout } from 'antd'
-import { Link } from "@reach/router"
+const TermsOfUse = (props) => {
+    return (
+        <Modal
+            bodyStyle={{ fontSize: '10px' }}
+            title="Terms and Conditions"
+            visible={props.showTerms}
+            onOk={props.handleOk}
+            onCancel={props.hideTerms}
+        >
+            <h2><strong>Welcome to poddhype.com</strong></h2>
 
+            <h2><strong>iFrames</strong></h2>
 
-const { Content } = Layout;
+            <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
 
+            <h2><strong>Cookies</strong></h2>
 
-const logo = require('./../../common/assets/poddhype-logo-blackandwhite.png');
+            <p>We employ the use of cookies. By accessing poddhype.com, you agreed to use cookies in agreement with the Poddhype's Privacy Policy.</p>
 
-class TermsOfUse extends React.Component {
-
-    render() {
-        return (
-            <Layout style={{
-                height: '100vh',
-            }}>
-                <Content className='content' style={{ padding: '40px' }}>
-
-                    <h2><strong>Terms and Conditions</strong></h2>
-
-                    <p>Welcome to poddhype.com</p>
-
-                    <p>These terms and conditions outline the rules and regulations for the use of Poddhype's Website, located at https://poddhype.com.</p>
-
-                    <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
-
-                    <h3><strong>Cookies</strong></h3>
-
-                    <p>We employ the use of cookies. By accessing poddhype.com, you agreed to use cookies in agreement with the Poddhype's Privacy Policy.</p>
-
-                    <h3><strong>License</strong></h3>
-
-                    <p>You must not:</p>
-                    <ul>
-                        <li>Republish material from poddhype.com</li>
-                        <li>Sell, rent or sub-license material from poddhype.com</li>
-                        <li>Reproduce, duplicate or copy material from poddhype.com</li>
-                        <li>Redistribute content from poddhype.com</li>
-                    </ul>
-
-                    <h3><strong>iFrames</strong></h3>
-
-                    <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
-
-                    <h3><strong>Your Privacy</strong></h3>
-                    <p> We store the information you enter in the sign up. This information is used in our platform to show your profile and match you with brands that use the portal.
-                        Your email will not be shared or displayed on the website. We will not sell your data to any third party. We have the right to use your podcast name and podcast image in marketing for poddhype.com unless you have told us otherwise.
-                        You can at any time tell us you don't want us to store your information, you can do so by emailing hej@podhype.com.
-                    </p>
-
-                </Content>
-            </Layout >
-        )
-    }
+            <h2><strong>Privacy Policy</strong></h2>
+            <h2> What information do we collect?</h2>
+            <p>In Short: We collect personal information that you provide to us such as name, address, contact information, passwords and security data, and information about your podcast that you enter in the signup.
+            We collect personal information that you voluntarily provide to us when registering at the Services expressing an interest in obtaining information about us or our products and services, when participating in activities on the Services (such as posting messages in our online forums or entering competitions, contests or giveaways) or otherwise contacting us.
+            The personal information that we collect depends on the context of your interactions with us and the Services, the choices you make and the products and features you use. The personal information we collect can include the following:
+            Personal Information Provided by You. We collect app usage, and other similar data.
+            Credentials. We collect passwords, password hints, and similar security information used for authentication and account access.
+            All personal information that you provide to us must be true, complete and accurate, and you must notify us of any changes to such personal information.
+            We store cookies for analytics on how our users use our site and to keep track if you are a returning visitor.
+            </p>
+            <h2> Why do we store this information? </h2>
+            <p> Your information is used on our platform to show display your profile. Your email will not be shared or displayed on the website.
+                We will not sell your data to any third party. We have the right to use your podcast name and podcast image in marketing for poddhype.com unless you have told us otherwise.
+                You can at any time tell us you don't want us to store your information, you can do so by sening an email to info@podhype.com.
+            </p>
+        </Modal>
+    )
 }
 
 export default TermsOfUse;
